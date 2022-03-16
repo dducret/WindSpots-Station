00f73d5e5a0fe925428ad6832f6e37c136864e28
Denis Ducret <denis.ducret@sdic.ch>
2022-03-16 08:04:54 +0100

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta name="robots" content="noindex,nofollow"/>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style type="text/css">
+  img         { vertical-align: middle; border-style: none; padding-bottom: 5px }       
+  .normal     { font-family: Arial, Helvetica, sans-serif; font-size: 10pt; font-weight: normal; font-style: normal }
+  .style1     { font-family: Arial, Helvetica, sans-serif; font-size: 18pt; font-weight: bold; color: #009900 }
+  </style>
+  <title>WindSpots Station</title>
+</head>
+<body>
+  <div align="center"><img src="logo.png" alt="logo" /></div>
+  <div align="center" class="style1">WindSpots Station</div>
+  <div class="normal">
+    <br/>Version 1.4 &copy; WindSpots.org 2022<br/>
+  </div>
+</body>
+</html>
